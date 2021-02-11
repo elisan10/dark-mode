4992fbf7097f26592dd9c5fa6de2f1a255e6c842
Eli <elize.san10@gmail.com>
2021-02-10 18:44:11 -0600

    STEP 2 completed - create useDarkMode function STEP 3 completed - imported dark mode in app
    MVP met

---
+++ src/components/hooks/useDarkMode.js
@@ -0,0 +1,8 @@
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = () => {
+  const [darkMode, setDarkMode] = useLocalStorage("darkMode", false);
+  return [darkMode, setDarkMode];
+};
+
+export default useDarkMode;
